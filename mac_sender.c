6aad76e1fb6d81e75d83e95b83c5fcd327edbe41
73jn <jean.nanchen@gmail.com>
2021-03-30 14:33:39 +0200

    Working on mac sender, send token

--- mac_sender.c
+++ mac_sender.c
@@ -12,13 +12,57 @@
 #include "main.h"
 #include "ext_led.h"
 
-
+void sendToken(osStatus_t	retCode,	struct queueMsg_t* queueMsg, char* tokenTag,	char * msg){
+	//Send to LCD TOKEN_LIST
+	
+	
+	//Send to Physical
+	
+	//------------------------------------------------------------------------
+	// MEMORY ALLOCATION				
+	//------------------------------------------------------------------------
+	msg = osMemoryPoolAlloc(memPool,osWaitForever);				
+	queueMsg->type = TOKEN;
+	queueMsg->anyPtr = msg;
+	memcpy(msg,tokenTag,strlen(tokenTag)+1);
+		//------------------------------------------------------------------------
+		// QUEUE SEND								
+		//------------------------------------------------------------------------
+		retCode = osMessageQueuePut(
+			queue_phyS_id,
+			queueMsg,
+			osPriorityNormal,
+			osWaitForever);
+		CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);					
+}
 //////////////////////////////////////////////////////////////////////////////////
 // THREAD MAC RECEIVER
 //////////////////////////////////////////////////////////////////////////////////
 void MacSender(void *argument)
 {
-
+	char * msg;
+	char tokenTag = TOKEN_TAG;
+	osStatus_t	retCode; //Message de retour de la queue
+	struct queueMsg_t queueMsg;	// message queue
+	for (;;){
+		//----------------------------------------------------------------------------
+		// QUEUE READ										
+		//----------------------------------------------------------------------------
+		retCode = osMessageQueueGet( 	
+			queue_macS_id,
+			&queueMsg,
+			NULL,
+			osWaitForever); 	
+    CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);		
+		
+		
+		switch(queueMsg.type){
+			case NEW_TOKEN:
+				sendToken(retCode, &queueMsg, &tokenTag, msg);
+				break;
+			
+			default :
+				
+		}
+	}
 }
-
-
