8e3a5aa8fbce851d3e0c5b64a19664da72f815e1
Laurasid <42735600+Laurasid@users.noreply.github.com>
2021-03-30 16:08:24 +0200

    Update mac_receiver.c

--- mac_receiver.c
+++ mac_receiver.c
@@ -27,12 +27,14 @@ void MacReceiver(void *argument)
 		//----------------------------------------------------------------------------
 		// QUEUE READ										
 		//----------------------------------------------------------------------------
+		// frame come always FROM_PHY
 		retCode = osMessageQueueGet( 	
 			queue_macR_id,
 			&queueMsg,
 			NULL,
 			osWaitForever); 	
-    CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);				
+    CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);	
+			
 		qPtr = queueMsg.anyPtr;
 		
 		switch(qPtr[0]){
@@ -46,14 +48,6 @@ void MacReceiver(void *argument)
 				osWaitForever);
 			
 				CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);	
-				break;
-			
-			case FROM_PHY :
-				break;
-			
-			default : 
-				int i;
-				i=0;
 				break;
 			
 		}
