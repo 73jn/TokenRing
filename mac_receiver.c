5168348f6602f85420cef8f9530bb10917f08413
Laurasid <42735600+Laurasid@users.noreply.github.com>
2021-04-01 14:27:02 +0200

    Dispatcher done - but not tested

--- mac_receiver.c
+++ mac_receiver.c
@@ -22,6 +22,7 @@ void MacReceiver(void *argument)
 	uint8_t * qPtr;
 	size_t	size;
 	osStatus_t retCode;
+	uint8_t src_addr;
 	
 	for(;;){
 		//----------------------------------------------------------------------------
@@ -32,25 +33,67 @@ void MacReceiver(void *argument)
 			queue_macR_id,
 			&queueMsg,
 			NULL,
-			osWaitForever); 	
+			osWaitForever); 		// thread is blocking while queue is empty
     CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);	
 			
 		qPtr = queueMsg.anyPtr;
+		src_addr = qPtr[0]>>3;
 		
-		switch(qPtr[0]){
+		if(qPtr[0] == TOKEN_TAG){
 			
-			case TOKEN_TAG :
-				//put token frame in queue_macS_id
-				retCode = osMessageQueuePut(
-				queue_macS_id,
-				&queueMsg,
-				osPriorityNormal,
-				osWaitForever);
-			
-				CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);	
-				break;
-			
+			//change type of message before dispatch it
+			queueMsg.type = TOKEN;
+			//put token frame in queue_macS_id
+			retCode = osMessageQueuePut(
+			queue_macS_id,
+			&queueMsg,
+			osPriorityNormal,
+			osWaitForever);	//blocking
+		
+			CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);	
 		}
+		else{
+			switch(src_addr){
+				case BROADCAST_ADDRESS :
+					//change type of message before dispatch it
+					queue.Msg.type = DATA_IND;
+					//put time frame in queue_timeR_id
+					retCode = osMessageQueuePut(
+					queue_timeR_id,
+					&queueMsg,
+					osPriorityNormal,
+					osWaitForever);	//blocking
+					break;
+				
+				case MYADDRESS :
+					//change type of message before dispatch it
+					queue.Msg.type = DATABACK;
+					//put time frame in queue_macS_id
+					retCode = osMessageQueuePut(
+					queue_macS_id,
+					&queueMsg,
+					osPriorityNormal,
+					osWaitForever);	//blocking
+					break;
+				
+				default :		//source is another 
+					//change type of message before dispatch it
+					queue.Msg.type = DATA_IND;
+					//put time frame in queue_chatR_id
+					retCode = osMessageQueuePut(
+					queue_chatR_id,
+					&queueMsg,
+					osPriorityNormal,
+					osWaitForever);	//blocking
+					break;
+			}
+		}	
+		
+		//----------------------------------------------------------------------------
+		// MEMORY RELEASE	(created frame : phy layer style)
+		//----------------------------------------------------------------------------
+		retCode = osMemoryPoolFree(memPool,qPtr);
+		CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);	
 	}
 }
 
