7abd7e24a11b7a81761b0dd2bec4fa6a0cec866b
Laurasid <42735600+Laurasid@users.noreply.github.com>
2021-04-19 08:27:23 +0200

    Update Mac_receiver

--- mac_receiver.c
+++ mac_receiver.c
@@ -12,6 +12,47 @@
 #include "main.h"
 
 
+#define BIT_ACK 	0
+#define BIT_READ 	1
+
+
+void putTokenOnQueue(struct queueMsg_t msg){
+	//change type of message before dispatch it
+	queueMsg.type = TOKEN;
+	//put token frame in queue_macS_id
+	retCode = osMessageQueuePut(
+	queue_macS_id,
+	&msg,
+	osPriorityNormal,
+	osWaitForever);	//blocking
+
+	CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);		
+}
+
+void putInQueue(enum msgType_e type, struct queueMsg_t msg, osMessageQueueId_t queue){
+	//change type of message before dispatch it
+	queueMsg.type = type;
+	//put token frame in queue_macS_id
+	retCode = osMessageQueuePut(
+	queue,
+	&msg,
+	osPriorityNormal,
+	osWaitForever);	//blocking
+
+	CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);		
+}
+
+uint8_t calculateCRC(uint8_t* data){
+	uint8_t i = 0;
+	uint8_t temp = 0;
+	do{
+		temp += data[i];
+		i++;
+	}while(data[i] != '\0');
+	
+	return temp;
+}
+
 //////////////////////////////////////////////////////////////////////////////////
 // THREAD MAC RECEIVER
 //////////////////////////////////////////////////////////////////////////////////
@@ -22,79 +63,105 @@ void MacReceiver(void *argument)
 	uint8_t * qPtr;
 	size_t	size;
 	osStatus_t retCode;
+	
 	uint8_t src_addr;
+	uint8_t dst_addr;
+	uint8_t* status;
+	uint8_t msg_length;
+	
+	enum states{ST_IDLE, ST_RECEIVING, ST_WAIT_TRANS};
+	enum states new_state = ST_IDLE;
+	enum states old_state;
+	
 	
 	for(;;){
-		//----------------------------------------------------------------------------
-		// QUEUE READ										
-		//----------------------------------------------------------------------------
-		// frame come always FROM_PHY
-		retCode = osMessageQueueGet( 	
-			queue_macR_id,
-			&queueMsg,
-			NULL,
-			osWaitForever); 		// thread is blocking while queue is empty
-    CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);	
-			
-		qPtr = queueMsg.anyPtr;
-		src_addr = qPtr[0]>>3;
-		
-		if(qPtr[0] == TOKEN_TAG){
-			
-			//change type of message before dispatch it
-			queueMsg.type = TOKEN;
-			//put token frame in queue_macS_id
-			retCode = osMessageQueuePut(
-			queue_macS_id,
-			&queueMsg,
-			osPriorityNormal,
-			osWaitForever);	//blocking
-		
+		do{
+			//----------------------------------------------------------------------------
+			// QUEUE READ										
+			//----------------------------------------------------------------------------
+			// frame come always FROM_PHY
+			retCode = osMessageQueueGet( 	
+				queue_macR_id,
+				&queueMsg,
+				NULL,
+				osWaitForever); 		// thread is blocking while queue is empty
 			CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);	
-		}
-		else{
-			switch(src_addr){
-				case BROADCAST_ADDRESS :
-					//change type of message before dispatch it
-					queue.Msg.type = DATA_IND;
-					//put time frame in queue_timeR_id
-					retCode = osMessageQueuePut(
-					queue_timeR_id,
-					&queueMsg,
-					osPriorityNormal,
-					osWaitForever);	//blocking
-					break;
 				
-				case MYADDRESS :
-					//change type of message before dispatch it
-					queue.Msg.type = DATABACK;
-					//put time frame in queue_macS_id
-					retCode = osMessageQueuePut(
-					queue_macS_id,
-					&queueMsg,
-					osPriorityNormal,
-					osWaitForever);	//blocking
-					break;
-				
-				default :		//source is another 
-					//change type of message before dispatch it
-					queue.Msg.type = DATA_IND;
-					//put time frame in queue_chatR_id
-					retCode = osMessageQueuePut(
-					queue_chatR_id,
-					&queueMsg,
-					osPriorityNormal,
-					osWaitForever);	//blocking
-					break;
+			//get frame
+			qPtr = queueMsg.anyPtr;
+			//get source address
+			src_addr = qPtr[0]>>3;	
+			//get dest address
+			dst_addr = qPtr[1]>>3;		
+			
+			if(qPtr[0] == TOKEN_TAG){
+				putTokenOnQueue(queueMsg);			
 			}
-		}	
+			else{
+				//get msg from frame
+				msg = &qPtr[3];		//pointer to first char of msg
+				//get length from frame
+				msg_length = qPtr[2];
+				//get status from frame
+				status = msg + msg_length;
+			
+				if(src_addr == MYADDRESS){
+					//databack
+					if(status[BIT_READ] == 1){	//READ bit = 1
+						if(status[BIT_ACK] == 1){	//ACK bit = 1
+							//put TOKEN in mac sender queue
+							putTokenOnQueue(queueMsg);
+						}
+						else{	//ACK bit = 0
+							//reset READ and ACK bits
+							status[BIT_ACK] = 0;
+							status[BIT_READ] = 0;
+							//put data in mac sender queue 
+							putInQueue(DATABACK, queueMsg, queue_macS_id);
+							//put token frame in queue_macS_id
+							
+							
+						}
+					}
+					else{	//READ bit = 0
+						//MAC_ERROR_INDICATION
+						
+						//put TOKEN in mac sender queue
+						putTokenOnQueue(queueMsg);
+					}
+				}
+				else{
+					if(dst_addr == MYADDRESS){
+						//compute CRC
+						if(calculateCRC(&msg) == status&&0xFC){
+							//set READ and ACK bits
+							status[BIT_ACK] = 1;
+							status[BIT_READ] = 1;
+							//put message in chat queue + MAC_DATA_INDICATION (?)
+							putInQueue(DATA_IND, msg, queue_chatR_id);
+							
+						}
+						else{
+							//set READ bit only (ACK = 0)
+							status[BIT_ACK] = 0;
+							status[BIT_READ] = 1;
+							//resend data to sender
+							//putInQueue(type,message,queue);
+						}
+					}
+					else{
+						//broadcast data
+					}
+				}
+			}
+		}while(/*queue_macR_id is not empty*/);	
 		
 		//----------------------------------------------------------------------------
 		// MEMORY RELEASE	(created frame : phy layer style)
 		//----------------------------------------------------------------------------
+		/*
 		retCode = osMemoryPoolFree(memPool,qPtr);
-		CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);	
+		CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);
+		*/
 	}
 }
-
-
